2da872085db3e8c3f577bd36fdd016520f2d6d53
Ramazi <sazuashviliramazi@gmail.com>
2024-06-25 20:38:09 +0400

    Some Changings

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        grey_c: "#83869A",
+        orange_c: "#FF6F48",
+        pink_c: "#F02AA6"
+      },
+    },
+  },
+  plugins: [],
+}
+
